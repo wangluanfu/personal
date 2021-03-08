c9316da1cde9dc7e6cba17bac226dd2d1aa44bd7
wangluanfu <79845092+wangluanfu@users.noreply.github.com>
2021-03-08 15:47:09 +0800

    Add files via upload
    提交内容

---
+++ src/test/java/com/apply/passageWay/personal/PersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apply.passageWay.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
