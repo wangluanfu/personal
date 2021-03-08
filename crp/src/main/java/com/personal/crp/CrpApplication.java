28eed284d8ebce68e61f5836ea43cbcd91719865
wangluanfu <79845092+wangluanfu@users.noreply.github.com>
2021-03-08 16:03:20 +0800

    Add files via upload
    提交

---
+++ crp/src/main/java/com/personal/crp/CrpApplication.java
@@ -0,0 +1,13 @@
+package com.personal.crp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrpApplication.class, args);
+    }
+
+}
---
+++ crp/src/test/java/com/personal/crp/CrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.crp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
